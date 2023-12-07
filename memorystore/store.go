fc50a35ed5cd0b16637397146bc9950cf717d968
Chris Yang <yjunhao1111@gmail.com>
2023-12-07 15:20:39 -0500

    Fix in-memory and disk store

--- memorystore/store.go
+++ memorystore/store.go
@@ -1,35 +1,52 @@
 package memorystore
 
 import (
+	"sync"
+
 	"github.com/eventials/go-tus"
 )
 
 // MemoryStore implements an in-memory Store.
 type MemoryStore struct {
 	m map[string]string
+
+	mutex *sync.Mutex
 }
 
 // NewMemoryStore creates a new MemoryStore.
 func NewMemoryStore() (tus.Store, error) {
 	return &MemoryStore{
-		make(map[string]string),
+		m:     make(map[string]string),
+		mutex: &sync.Mutex{},
 	}, nil
 }
 
 func (s *MemoryStore) Get(fingerprint string) (string, bool) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	url, ok := s.m[fingerprint]
 	return url, ok
 }
 
 func (s *MemoryStore) Set(fingerprint, url string) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	s.m[fingerprint] = url
 }
 
 func (s *MemoryStore) Delete(fingerprint string) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	delete(s.m, fingerprint)
 }
 
 func (s *MemoryStore) Close() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	for k := range s.m {
 		delete(s.m, k)
 	}
